88f8a65cdec7e6ab40fbe06f39819eef7e54238e
PetyaKatsarova <pskpetya@gmail.com>
2021-11-23 17:05:13 +0100

    added solutions for problem 04 of lesson 8.Exercise: Remote Data and Authentication

--- 8.remote_data_and_auth_exercise/04.Book-library/app.js
+++ 8.remote_data_and_auth_exercise/04.Book-library/app.js
@@ -3,106 +3,129 @@ console.log('My requests...')
 //Get Book: This functionality is not needed in this task, but you can try it with postman by sending request to 'GET'the Book with id:' d953e5fb-a585-4d6b-92d3-ee90697398a0'. Send a GET request to this URL:http://localhost:3030/jsonstore/collections/books/:id Create Book Write functionality to create a new book, when the submit button is clicked. Before sending the request be sure the fields are not empty (make validation of the input). To create a book, you have to send a 'POST' request and the JSON body should be in the following format:{'author': 'New Author','title': 'New Title'}
 //Update Book By clicking the edit button of a book, change the form like this:The HTTP command 'PUT' modifies an existing HTTP resource. The URL is: http://localhost:3030/jsonstore/collections/books/:id The JSON body should be in the following format: {'author': 'Changed Author','title': 'Changed Title'} Delete Book By clicking the delete button you have to delete the book, without any confirmation. To delete a book use'DELETE' command and send REQUEST: http://localhost:3030/jsonstore/collections/books/:id
 
-function attachEvents(){
-    document.querySelector('#loadBooks').addEventListener('click', loadAllBooks);
-    document.querySelector('form').addEventListener('submit', (e) => {
-        e.preventDefault();
-        addBook();
-    });
-    
-}
-attachEvents();
+// caching: create obj state.books = books; //(from fetch) and then when we need elsewhere info for a book: no need for a req,
+// just access the state obj
 
-async function addBook(){
-    const title = document.querySelector('input[name=title]').value;
-    const author = document.querySelector('input[name=author]').value;
-    if(title == ''){
-        alert('Please fill in the book title');
-        return;
-    }
-    if(author == ''){
-        alert('Please fill in the book\'s author');
-        return;
-    }
-   
-    const options = {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({title, author})
-    };
-    const req = await fetch(`http://localhost:3030/jsonstore/collections/books`, options);
 
-    if(req.ok == false){
-        return alert(req.message);
+async function request(url, options){
+    const res = await fetch(url, options);
+    if(res.ok != true){
+        const error = await res.json();
+        alert(error.message);
+        throw new Error(error.message);
     }
-    
-    const data = await req.json();
-}
-
-async function loadAllBooks(){
-    const res = await fetch(`http://localhost:3030/jsonstore/collections/books`);
     const data = await res.json();
-    console.log(Object.values(data));
-    document.querySelector('tbody').innerHTML = '';
+    return data;
+}
+//func to load all books from servr and display them
+async function getAllBooks(){
+    const books = await request(`http://localhost:3030/jsonstore/collections/books`);
+    const rows = Object.entries(books).map(createRow).join('');
+    document.querySelector('tbody').innerHTML = rows;
 
-    Object.entries(data).map(arr => {
-        document.querySelector('tbody').appendChild(displayBook(arr[1], arr[0]));
-        // console.log(arr[0], arr[1]);
-    });
-
-    document.querySelectorAll('.delete').forEach(btn => {
-        btn.addEventListener('click',  delFromLibrary);
-    });
+    function createRow([id,book]){
+        const result = `
+        <tr data-id="${id}">
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>
+               <button class="editBtn">Edit</button>
+               <button class="deleteBtn">Delete</button>
+            </td>
+        </tr>`;
+        return result;
+    }
 }
 
-async function delFromLibrary(e){
-    const titleEl = e.target.parentElement.parentElement.querySelector('.title');
-    const bookId = titleEl.getAttribute('data-id');
-    const options = {
-        method: 'delete',
+async function createBook(e){
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const book = {
+        title: formData.get('title'), 
+        author: formData.get('author')
+    }
+   const result = await request(`http://localhost:3030/jsonstore/collections/books`, {
+       method: 'post',
+       headers: {'Content-Type': 'application/json'},
+       body: JSON.stringify(book)
+   });
+   e.target.reset(); // clears all the form fields
+    getAllBooks();
+}
+
+async function updateBook(e){
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const id = formData.get('id');
+    console.log(id);
+    const book = {
+        title: formData.get('title'),
+        author: formData.get('author')
+    }
+    const result = await request(`http://localhost:3030/jsonstore/collections/books/`+id, {
+        method: 'put',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({title, author})
-    };
+        body: JSON.stringify(book)
+    });
 
+    document.getElementById('createForm').style.display = 'block';
+    document.getElementById('editForm').style.display = 'none';
+    e.target.reset();
+    getAllBooks();
+}
 
-    const req = await fetch(`http://localhost:3030/jsonstore/collections/books`+bookId, options);
+async function deleteBook(id){
+    await request(`http://localhost:3030/jsonstore/collections/books/`+id, {
+        method: 'delete'
+    });
+    getAllBooks();
+}
 
-    if(req.ok == false){
-        return alert(req.message);
+function start(){
+    document.getElementById('loadBooks').addEventListener('click', getAllBooks);
+    document.getElementById('createForm').addEventListener('submit', createBook);
+
+    document.getElementById('editForm').addEventListener('submit', updateBook);
+    document.querySelector('table').addEventListener('click', handleTableClick);
+    document.querySelector('#editForm input[type="button"]').addEventListener('click', (e)=>{
+        document.getElementById('createForm').style.display = 'block';
+        document.getElementById('editForm').style.display = 'none';
+        e.target.parentElement.reset();
+    });
+
+    document.querySelector('table').addEventListener('click', handleTableClick);
+
+    getAllBooks();
+}
+
+start();
+
+function handleTableClick(e){
+    if(e.target.className == 'editBtn'){
+        document.getElementById('createForm').style.display = 'none';
+        document.getElementById('editForm').style.display = 'block';
+        
+        const bookId = e.target.parentNode.parentNode.dataset.id;
+        loadBookForEditting(bookId);
+         
+    }else if(e.target.className == 'deleteBtn'){
+        const confirmed = confirm('R u sure u want to delete this book?');
+        if(confirmed){
+            const bookId = e.target.parentNode.parentNode.dataset.id;
+            deleteBook(bookId);
+        }
     }
+}
+
+async function loadBookForEditting(id){
+    const book = await request(`http://localhost:3030/jsonstore/collections/books/`+id);
     
-    const data = await req.json();
-
+    document.querySelector('#editForm [name="title"]').value = book.title;
+    document.querySelector('#editForm [name="author"]').value = book.author;
+    document.querySelector('#editForm [name="id"]').value = id;
 }
 
-function displayBook(obj, bookId){
-    const tr = createEl('tr', undefined);
-    const title = createEl('td', obj.title, ['class=title']);
-    title.setAttribute('data-id', bookId);
-    const author = createEl('td', obj.author, ['class=author']);
-    const wrapper = createEl('td', undefined, ['class=wrapper']);
-    const edit = createEl('button', 'Edit', ['class=edit']);
-    const del = createEl('button', 'Delete', ['class=delete']);
 
-    wrapper.appendChild(edit);
-    wrapper.appendChild(del);
-    tr.appendChild(title);
-    tr.appendChild(author);
-    tr.appendChild(wrapper);
-    return tr;
-}
 
-function createEl(el, text, arrAttr){
-    let elem = document.createElement(el);
-    if(text !== undefined){
-        elem.textContent = text;
-    }
-    if(arrAttr){ //['class=kuku', 'style=color:tomato']
-        arrAttr.forEach(item => {
-            let [prop, val] = item.split('=');
-            elem.setAttribute(prop, val);
-        });
-    }
 
-    return elem;
-}
+
